10d3c941d1d999cf9a6d8a8a4e7c7629104d8c52
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-09-01 14:37:10 -0500

    Auth with email and password

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,39 +1,51 @@
+const myForm = document.querySelector('form');
+
 // Verify what the host name is to assign it in the var url ternary.
 console.log('hostname:', window.location.hostname);
 // HTML
-var url = window.location.hostname.includes('localhost')
-  ? 'http://localhost:8080/api/auth/google'
-  : 'https://web-rest-server-nodejs.herokuapp.com/api/auth/google';
+const url = window.location.hostname.includes('localhost')
+  ? 'http://localhost:8080/api/auth/'
+  : 'https://web-rest-server-nodejs.herokuapp.com/api/auth/';
 
-// this function is the same "parseJwt(token)" that is in /function/parse-jwt.js
-// function decodeJwtResponse(token) {
-//   var base64Url = token.split('.')[1];
-//   var base64 = base64Url.replace('-', '+').replace('_', '/');
-//   return JSON.parse(window.atob(base64));
-// }
+myForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const formData = {};
+  for (let element of myForm.elements) {
+    if (element.name.length > 0) {
+      formData[element.name] = element.value;
+      // console.log('formData', formData);
+    }
+  }
+
+  fetch(url + 'login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+body: JSON.stringify(formData), // The body arguments are email and password but they are already serialized in formData
+  })
+    .then(res => res.json())
+    .then(({msg, token}) => {
+      if (msg) {
+        return console.error(msg);
+      }
+      localStorage.setItem('token', token)
+    })
+    .catch(console.log);
+});
 
 // This is the function that calls the google button in the html.
-function handleCredentialResponse(response) {
+function googleSignIn(response) {
   // In the response comes the data of the google user.
   const { credential: token_google } = response;
-
-  // User data from Google
-  // const userData = decodeJwtResponse(token_google);
-  // console.log(userData);
-
-  // console.log('ID: ' + userData.sub);
-  // console.log('Full Name: ' + userData.name);
-  // console.log('Given Name: ' + userData.given_name);
-  // console.log('Family Name: ' + userData.family_name);
-  // console.log('Image URL: ' + userData.picture);
-  // console.log('Email: ' + userData.email);
 
   // id_token is necessary to pass it as an argument in the body of the fetch
   const id_token = { id_token: token_google };
   // console.log('token google elis:', id_token);
 
   // Doing a fetch to the backend sending the google token to return the user's data from the database.
-  fetch(url, {
+  fetch(url + 'google', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -44,7 +56,8 @@ function handleCredentialResponse(response) {
     .then(({ token }) => {
       // In the argument the data returned by the backend was destructured
       // console.log('data fetch response:', data);
-      console.log('Token from my backend:', { token }); // {token: "eazAEa..."}
+      // console.log('Token from my backend:', { token }); // {token: "eazAEa..."}
+      localStorage.setItem('token', token);
     })
     .catch(console.log);
 }
