c32709bef266918b741a03cd4629c614698c23a4
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-09-01 12:59:04 -0500

    Google Auth

---
+++ public/js/auth.js
@@ -0,0 +1,49 @@
+// Verify what the host name is to assign it in the var url ternary.
+console.log('hostname:', window.location.hostname);
+// HTML
+var url = window.location.hostname.includes('localhost')
+  ? 'http://localhost:8080/api/auth/google'
+  : 'https://web-rest-server-nodejs.herokuapp.com/api/auth/google';
+
+function decodeJwtResponse(token) {
+  var base64Url = token.split('.')[1];
+  var base64 = base64Url.replace('-', '+').replace('_', '/');
+  return JSON.parse(window.atob(base64));
+}
+
+// This is the function that calls the google button in the html.
+function handleCredentialResponse(response) {
+  // In the response comes the data of the google user.
+  const { credential: token_google } = response;
+
+  // User data from Google
+  const userData = decodeJwtResponse(token_google);
+  // console.log(userData);
+
+  console.log('ID: ' + userData.sub);
+  console.log('Full Name: ' + userData.name);
+  console.log('Given Name: ' + userData.given_name);
+  console.log('Family Name: ' + userData.family_name);
+  console.log('Image URL: ' + userData.picture);
+  console.log('Email: ' + userData.email);
+
+  // id_token is necessary to pass it as an argument in the body of the fetch
+  const id_token = { id_token: token_google };
+  // console.log('token google elis:', id_token);
+
+  // Doing a fetch to the backend sending the google token to return the user's data from the database.
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(id_token),
+  })
+    .then(res => res.json())
+    .then(({ token }) => {
+      // In the argument the data returned by the backend was destructured
+      // console.log('data fetch response:', data);
+      console.log('Token from my backend:', { token }); // {token: "eazAEa..."}
+    })
+    .catch(console.log);
+}
--- sockets/controller.js
+++ sockets/controller.js
@@ -2,7 +2,7 @@ const { Socket } = require('socket.io');
 
 // TODO: I will have delete "new Socket()" in the parameters
 const socketController = (socket = new Socket()) => {
-  console.log('client connected', socket.id);
+  
 };
 
 module.exports = {
